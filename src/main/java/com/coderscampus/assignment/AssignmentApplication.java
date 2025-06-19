c19fd2092588b57f0ce0d571f1f2566f0e33cfff
RTK <126328596+rtk666a@users.noreply.github.com>
2025-06-19 11:41:03 -0600

    remove the comment

--- src/main/java/com/coderscampus/assignment/AssignmentApplication.java
+++ src/main/java/com/coderscampus/assignment/AssignmentApplication.java
@@ -1,13 +1,7 @@
 package main.java.com.coderscampus.assignment;
 
 
-
-import java.util.List;
-import java.util.Map;
-
-
-
-public class  AssignmentApplication {
+public class AssignmentApplication {
     public static void main(String[] args) {
         var service = new AssigmentService();
         service.printResults(service.countOccurrences(service.fetchNumbers()));
